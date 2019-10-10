04bd96d7d4772f847116896f46efac8668f0dee0
Nehal Jethwa <nehaljethwa6@gmail.com>
2019-10-10 12:20:48 +0530

    Updates

---
+++ precache-manifest.6f7b11f7033a27742dee825e42d9f207.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36cce171e4a79e8b6dd49699d51817ce",
+    "url": "/space-x/index.html"
+  },
+  {
+    "revision": "22f234802de8338b987d",
+    "url": "/space-x/static/css/main.a6e4ab61.chunk.css"
+  },
+  {
+    "revision": "2e5e424c41a68953a471",
+    "url": "/space-x/static/js/2.a0d5587f.chunk.js"
+  },
+  {
+    "revision": "22f234802de8338b987d",
+    "url": "/space-x/static/js/main.a30cc6a7.chunk.js"
+  },
+  {
+    "revision": "79d9c641a1fb43b57414",
+    "url": "/space-x/static/js/runtime-main.b2ac0d93.js"
+  },
+  {
+    "revision": "9174ed1c71a23bf0c435bd0c03885e41",
+    "url": "/space-x/static/media/error-404.9174ed1c.svg"
+  }
+]);
